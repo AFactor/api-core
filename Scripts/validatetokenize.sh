4c98ded645855dfcf31933aa6f27a1da2a0fa6f8
AvikS <avik.sengupta@royalmail.com>
2018-03-05 16:48:39 +0000

    x

--- Scripts/validatetokenize.sh
+++ Scripts/validatetokenize.sh
@@ -3,6 +3,6 @@
 export PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/bin:/usr/local/apache-maven/apache-maven-3.3.9:/Users/Shared/Jenkins/Home/tools/jenkins.plugins.nodejs.tools.NodeJSInstallation/latest/lib/node_modules
 echo node -v
 echo 'avik'
-b=$(currentBuild.changeSets)
+b=${currentBuild.changeSets}
 npm install --prefix core/Scripts
 node core/Scripts/tokenize.js $b
