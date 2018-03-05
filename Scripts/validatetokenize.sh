6946fd6df608b216ba79ba9ffae478acecef9371
AvikS <avik.sengupta@royalmail.com>
2018-03-05 12:06:49 +0000

    x

---
+++ Scripts/validatetokenize.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/bin:/usr/local/apache-maven/apache-maven-3.3.9:/Users/Shared/Jenkins/Home/tools/jenkins.plugins.nodejs.tools.NodeJSInstallation/latest/lib/node_modules
+echo node -v
+npm install
+node tokenize.js
