099b0c63f590c90162412d443bdf4878cc81034a
AvikS <avik.sengupta@royalmail.com>
2018-03-05 13:52:11 +0000

    two more shs

---
+++ Scripts/deploy.sh
@@ -0,0 +1,3 @@
+export PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/bin:/usr/local/apache-maven/apache-maven-3.3.9:/Users/Shared/Jenkins/Home/tools/jenkins.plugins.nodejs.tools.NodeJSInstallation/latest/lib/node_modules
+b=$(git diff --name-only HEAD^ HEAD)
+node core/scripts/publish.js ${BUILD_TAG} $ciConfig $b
