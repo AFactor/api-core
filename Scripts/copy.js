39dc1dd6ca5d67e4deaa62994be842f7de3cb6d9
AvikS <avik.sengupta@royalmail.com>
2018-03-06 17:32:00 +0000

    x

--- Scripts/copy.js
+++ Scripts/copy.js
@@ -7,14 +7,14 @@ var
 
 
 
-console.warn('changelog: ' + process.argv[3]);
+console.warn('args: ' + process.argv);
 console.warn('now creating a tmp sub folder');
 var folderName = util.format("tmp_%s_%s", process.argv[2], new Date().getTime());
 fs.mkdirSync(folderName);
 console.warn('tmp folder ' + folderName + ' created');
 console.warn('copy all apis in the subfolder');
 cp.sync('./apis/**/*.yaml', "./" + folderName);
-console.warn('now copy relevant products');
+console.warn('now copy relevant products, if any');
 glob("./products/*.yaml", null, function (er, files) {
   for(var f in files){
     var fileName = files[f].split("/")[files[f].split("/").length-1];
