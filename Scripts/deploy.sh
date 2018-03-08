d3e57a5b8e27a35224a4a86ee66add048cdda708
AvikS <avik.sengupta@royalmail.com>
2018-03-08 14:30:58 +0000

    sh

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -1,2 +1,2 @@
 export PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/bin:/usr/local/apache-maven/apache-maven-3.3.9:/Users/Shared/Jenkins/Home/tools/jenkins.plugins.nodejs.tools.NodeJSInstallation/latest/lib/node_modules
-node core/scripts/publish.js ${BUILD_TAG} $ciConfig
+node core/scripts/publish.js ${BUILD_TAG} $sitConfig
